00926d1d761421942a97c339a3dadb5ce0097cc7
Rohit981 <46937934+Rohit981@users.noreply.github.com>
2022-10-12 19:44:58 +0530

    Beat Commit
    Added a Beat script which is an empty game object and added a prefab folder

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+    [SerializeField] private float BPM = 200;
+     private float beat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeBPM();
+
+        Time.timeScale = beat;
+    }
+
+    void ChangeBPM()
+    {
+        if (BPM <= 200 && BPM > 150)
+        {
+            beat = 1;
+        }
+        else if (BPM <= 150 && BPM > 110)
+        {
+            beat = 0.5f;
+
+        }
+    }
+}
